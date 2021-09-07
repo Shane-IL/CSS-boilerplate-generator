e7c1fe0b14ac1b60bf1cf64ac04ffe13c428a074
Shane <shanegoodson101@gmail.com>
2021-09-07 10:33:59 +0300

    converted into a cli module

---
+++ src/cli.js
@@ -0,0 +1,44 @@
+import arg from 'arg';
+import inquirer from 'inquirer';
+
+import { generateCSSBoilerplate } from './css-boilerplate-generator.js';
+
+function parseArguments(rawArgs) {
+    const args = arg({},{ argv: rawArgs.slice(2)});
+    return {
+      inputPath: args._[0],
+      outputPath: args._[1]
+    };
+}
+
+async function promptForMissingParams(params) {   
+    const questions = [];
+    if (!params.inputPath) {
+      questions.push({
+        type: 'input',
+        name: 'inputPath',
+        message: 'Please enter an input path'
+      });
+    }
+
+    if (!params.outputPath) {
+        questions.push({
+          type: 'input',
+          name: 'outputPath',
+          message: 'Please enter an output path'
+        });
+    }
+     
+    const answers = await inquirer.prompt(questions);
+    return {
+      ...params,
+      inputPath: params.inputPath || answers.inputPath,
+      outputPath: params.outputPath || answers.outputPath,
+    };
+   }
+
+export async function cli(args) {
+    let parsedArgs = parseArguments(args);
+    parsedArgs = await promptForMissingParams(parsedArgs);
+    generateCSSBoilerplate(parsedArgs.inputPath, parsedArgs.outputPath);
+}
---
+++ src/css-boilerplate-generator.js
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import readline from 'readline';
+
+const classesRegex = /class="([a-z0-9 -])+"/;
+const idsRegex = /id="([a-z0-9 -])+"/;
+
+export async function generateCSSBoilerplate(inputPath = "./index.html", outputPath = "./styles.css", defaultSelectors = [
+    'html, body',
+    'body',
+    '*'
+]) {
+    const fileStream = fs.createReadStream(inputPath);
+
+    const rl = readline.createInterface({
+        input: fileStream
+    });
+
+    const ansArr = [];
+
+    for await (const line of rl) {
+        const idMatches = line.match(idsRegex);
+        if(idMatches && idMatches.length > 0)ansArr.push("#"+idMatches[0].substring(idMatches[0].indexOf('"') + 1, idMatches[0].lastIndexOf('"')));
+
+        const classMatches = line.match(classesRegex);
+        if(classMatches && classMatches.length > 0) {
+            const classString = classMatches[0].substring(classMatches[0].indexOf('"') + 1, classMatches[0].lastIndexOf('"'));
+            const classArray = classString.split(" ");
+            for(let className of classArray) {
+                ansArr.push("."+className);
+            }
+        }
+    }
+
+    const ansSet = new Set(ansArr.sort());
+
+    fs.writeFile(outputPath, '', err=> {
+        if(err) {
+            console.error(err)
+        } else {
+            console.log("file created");
+            const writeStream = fs.createWriteStream(outputPath);
+            console.log("writing default selectors");
+            defaultSelectors.forEach(selector => writeCSSEntry(writeStream, selector));
+            console.log("writing class selectors");
+            ansSet.forEach(selector => writeCSSEntry(writeStream, selector));
+            writeStream.end();
+        }
+    });
+}
+
+function writeCSSEntry(ws, selector) {
+    ws.write(`${selector} {\n\n\n}\n\n`);
+}
+
